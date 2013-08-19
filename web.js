84c3ba45e4a1a60c65a0b186d00aceef1c0b01c0
abhi1991 <abhishek.raj1991@gmail.com>
2013-08-19 18:55:16 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2! Abhishek');
-});
-
+  response.send('');
+    fs.readFile('index.html');
+	buf.toString();
+}
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
