13f6c6980213359c4677527e6d25dcb156f142c3
abhi1991 <abhishek.raj1991@gmail.com>
2013-08-19 20:07:25 +0000

    updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(hello);
-   var buffer = fs.readFile('/index.html');
+   var fs = require('fs');
+   var buffer = new Buffer(fs.readFileSync('index.html','utf8'));
    var hello =buffer.toString();
+   response.send(hello);
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
